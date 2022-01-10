ecc57564001a1436fdb6e3a515626bf101a60bfe
Tim O'Ryan <tim.oryan@bbc.co.uk>
2022-01-10 23:05:59 +0000

    added docstraings to JiraTicket classes

--- commstool3/flaskapp/ct3.py
+++ commstool3/flaskapp/ct3.py
@@ -43,21 +43,27 @@ _APIBASEURL: ParseResult = urllib.parse.urlparse(
 @dataclass(init=False)
 class JiraTicket:
     """
-    Class containing the basic elements of a Jira ticket
+    Class containing the basic elements of a Jira ticket.
+
+    Attributes:
+        api_url: The URL for the ticket in the Jira API, as a string
+        jira_url: The URL for the ticket in the Jira UI, as a ParseResult
+        ref: The Jira reference for the ticket, as a string
+        project: The Jira project to which the ticket belongs, as a string
     """
 
-    ticket_data: dict
+    _ticket_data: dict
     api_url: str
     jira_url: ParseResult
     ref: str
     project: str
 
     def __init__(self, ticket_data: dict) -> None:
-        self.ticket_data = ticket_data
-        self.api_url = self.ticket_data["self"]
-        self.jira_url = self.get_jira_url()
-        self.ref = self.ticket_data["key"]
-        self.project = self.ticket_data["key"].split("-")[0]
+        self._ticket_data = ticket_data
+        self.api_url = self._ticket_data["self"]
+        self.jira_url = self._get_jira_url()
+        self.ref = self._ticket_data["key"]
+        self.project = self._ticket_data["key"].split("-")[0]
         print(
             f"""\
             Project: {self.project}
@@ -69,9 +75,18 @@ class JiraTicket:
     @classmethod
     def create(cls, ref: str, api: ParseResult = _APIBASEURL):
         """
-        Static method used to create an instance of a JiraTicket or subclass.
-        Returns an OpsIncidentTicket object if project is OPS and issuetype is Incident,
-        returns JiraTicket otherwise.
+        Creates an instance of a JiraTicket or subclass of JiraTicket.
+
+        Use this method rather than creating an instance directly to ensure the correct
+        subclass is returned (ie. OpsIncidentTicket for an OPS incident).
+
+        Args:
+            ref: A string containing the Jira reference of the ticket to be created
+            api: The base URL of the Jira API, as urllib.parse.ParseResult
+
+        Returns:
+            An OpsIncidentTicket object if project is OPS and issuetype is Incident,
+            otherwise a JiraTicket object.
         """
         fetch_url = api.geturl() + f"issue/{ref}"
         response = _SESSION.get(fetch_url, verify=True)
@@ -85,11 +100,29 @@ class JiraTicket:
         else:
             return JiraTicket(data)
 
-    def get_jira_url(self) -> ParseResult:
-        url = urllib.parse.urlparse(self.api_url)
-        return url._replace(path=f"browse/{self.ticket_data['key']}")
+    def _get_jira_url(self) -> ParseResult:
+        """
+        Gets the URL for a ticket in the Jira UI.
 
-    def update_from_jira(self):
+        The Jira API does not provide this itself, so we have to infer it.
+
+        Returns:
+            The URL for the ticket in the Jira UI, as a ParseResult
+        """
+        url = urllib.parse.urlparse(self.api_url)
+        return url._replace(path=f"browse/{self._ticket_data['key']}")
+
+    def update_from_jira(self) -> None:
+        """
+        Refreshes the ticket object with data from Jira.
+
+        Returns:
+            None. Updates the object variables _ticket_data and jira_url
+
+        Raises:
+            ValueError if the project part of the key does not match the current
+            project, as this indicates the ticket has moved between projects.
+        """
         response = _SESSION.get(self.api_url)
         response.raise_for_status()  # Stop on HTTP error
         # Check that the ticket has not moved projects (project in response does not
@@ -98,15 +131,27 @@ class JiraTicket:
             raise ValueError(
                 f"Jira project does not match key {self.ref}. Has the ticket moved?"
             )
-        self.ticket_json = response.text
-        self.ticket_data = json.loads(self.ticket_json)
-        self.jira_url = self.get_jira_url()
+        ticket_json = response.text
+        self._ticket_data = json.loads(ticket_json)
+        self.jira_url = self._get_jira_url()
 
 
 @dataclass(init=False)
 class OpsIncidentTicket(JiraTicket):
     """
-    Subclass of JiraTicket with data relating to OPS incidents
+    Subclass of JiraTicket with additional data relating to OPS incidents
+
+    Attributes:
+        api_url:
+            The URL for the ticket in the Jira API, as a string
+        jira_url:
+            The URL for the ticket in the Jira UI, as a ParseResult
+        ref:
+            The Jira reference for the ticket, as a string
+        project:
+            The Jira project to which the ticket belongs, as a string
+        incident_fields:
+            Dictionary containing incident-specific fields, as defined in Config
     """
 
     incident_fields: dict
@@ -114,16 +159,16 @@ class OpsIncidentTicket(JiraTicket):
     def __init__(self, ticket_data):
         super().__init__(ticket_data)
         self.incident_fields = {}
-        self.incident_fields["Priority"] = self.ticket_data["fields"][
+        self.incident_fields["Priority"] = self._ticket_data["fields"][
             Config.CUSTOMFIELDS["incident_priority"]
         ]["value"]
         self.incident_fields["Start Time"] = datetime.strptime(
-            self.ticket_data["fields"][Config.CUSTOMFIELDS["incident_start_time"]],
+            self._ticket_data["fields"][Config.CUSTOMFIELDS["incident_start_time"]],
             Config.TIMEOPTIONS["jira_datetime_format"],
         )
         try:
             self.incident_fields["End Time"] = datetime.strptime(
-                self.ticket_data["fields"][Config.CUSTOMFIELDS["incident_end_time"]],
+                self._ticket_data["fields"][Config.CUSTOMFIELDS["incident_end_time"]],
                 Config.TIMEOPTIONS["jira_datetime_format"],
             )
         except TypeError:
@@ -133,31 +178,34 @@ class OpsIncidentTicket(JiraTicket):
 class JiraEmail:
     """
     Class representing an email to be sent
+
+    Attributes:
+        fromaddress: String containing the from email address
     """
 
     def __init__(self, fromaddress: str) -> None:
         self.fromaddress = fromaddress
-        self.message = MIMEMultipart(
+        self._message = MIMEMultipart(
             "alternative"
         )  # Create the email object for later use
 
     def populate_email(self, ticket: JiraTicket, template: Template) -> None:
         html = template.render(
             ref=ticket.ref,
-            summary=ticket.ticket_data["fields"]["summary"],
-            desc=ticket.ticket_data["fields"]["description"],
+            summary=ticket._ticket_data["fields"]["summary"],
+            desc=ticket._ticket_data["fields"]["description"],
         )
         part = MIMEText(html, "html")
-        self.message.attach(part)
+        self._message.attach(part)
 
     def add_recipients(self, recipients: list) -> None:
-        self.message["to"] = ",".join(recipients)
+        self._message["to"] = ",".join(recipients)
 
     def output_email(self, path: str, draft: bool = True) -> None:
         if draft:
-            self.message.add_header(
+            self._message.add_header(
                 "X-Unsent", "1"
             )  # Make the email appear unsent to Outlook
         with open(path, "w") as outfile:
             gen = generator.Generator(outfile)
-            gen.flatten(self.message)
+            gen.flatten(self._message)
